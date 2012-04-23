3738befab32fcea7b78c5f874de384974394ba2b
rm <rm@kirkeweb.dk>
2012-04-23 19:40:12 +0200

    Mesh, material and shaders for an "arm"

---
+++ materials/arm.material.js
@@ -0,0 +1,17 @@
+materials.arm = {
+		'frag_shaders' : ['arm.frag'],
+		'vert_shaders' : ['arm.vert'],
+		'attrib' : {
+			'aVertexPosition' : 0,
+			'aTextureCoord' : 0,
+			'aVertexNormal' : 0
+		},
+		'uniform' : {
+			'uPMatrix' : 0,
+			'uMVMatrix' : 0,
+		},
+		'textures' : {
+			
+		}
+	};
+	
---
+++ shaders/arm.frag
@@ -0,0 +1,9 @@
+precision mediump float;
+
+
+varying vec2 vTextureCoord;
+varying vec3 vNormal;
+
+void main(void) {
+	gl_FragColor = vec4(0.3, 0.9, 1.0, 1.0);
+}
